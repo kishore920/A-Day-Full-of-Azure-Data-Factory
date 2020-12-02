17f16feff3bdb48e4dd8e56fa22666bb883d1706
Paul Andrew <paul@mrpaulandrew.com>
2020-12-02 10:21:46 +0000

    Update Scale Out Demo Queries.sql

--- Demo Code/MetadataDB/Scripts/Scale Out Demo Queries.sql
+++ Demo Code/MetadataDB/Scripts/Scale Out Demo Queries.sql
@@ -1,9 +1,31 @@
-SELECT * FROM [dbo].[Buckets]
+--do this before demo:
+ALTER DATABASE [trainingdb01]
+MODIFY
+	(
+	EDITION = 'Standard',
+	SERVICE_OBJECTIVE = 'S6', --800 concurrent requests
+	MAXSIZE = 50 GB
+	)
 
-SELECT * FROM [dbo].[BucketProcesses]
+--for demo
+SELECT * FROM [dbo].[Buckets];
 
-SELECT * FROM [dbo].[BucketToProcessMap]
+SELECT * FROM [dbo].[BucketProcesses];
 
-EXEC [dbo].[SetBucketProcesses] @DebugMode = 1
+SELECT * FROM [dbo].[BucketToProcessMap];
 
-SELECT [SomeValue]  FROM [dbo].[DataDump]
+EXEC [dbo].[SetBucketProcesses] @DebugMode = 1;
+
+TRUNCATE TABLE [dbo].[DataDump];
+
+SELECT [SomeValue]  FROM [dbo].[DataDump];
+
+
+--after demo
+ALTER DATABASE [trainingdb01]
+MODIFY
+	(
+	EDITION = 'Standard',
+	SERVICE_OBJECTIVE = 'S2',
+	MAXSIZE = 50 GB
+	)
